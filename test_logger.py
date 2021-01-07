f40982963fad8520ad01783a52037340117ba256
marutoraman <33813153+marutoraman@users.noreply.github.com>
2021-01-07 16:10:06 +0900

    Update test_logger.py

--- test_logger.py
+++ test_logger.py
@@ -2,4 +2,10 @@ from logger import set_logger
 
 logger = set_logger(__name__)
 
-logger.info("info")
+def test():
+    logger.info("＊＊＊＊＊＊＊＊infoメッセージ＊＊＊＊＊＊＊＊＊＊")
+    logger.warning("＊＊＊＊＊＊＊＊warningメッセージ＊＊＊＊＊＊＊＊＊＊")
+    logger.error("＊＊＊＊＊＊＊＊errorメッセージ＊＊＊＊＊＊＊＊＊＊")
+    
+if __name__ == "__main__":
+    test()
