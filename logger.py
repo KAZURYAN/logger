fbeccf0259da6bb5b3cca0a06a2d70a5cbb08641
marutoraman <33813153+marutoraman@users.noreply.github.com>
2021-01-07 16:09:46 +0900

    Update logger.py

--- logger.py
+++ logger.py
@@ -7,7 +7,8 @@ LOG_DIR_NAME='logs'
 
 def set_logger(name):
     logger= logging.getLogger(name)
-    
+    logger.setLevel(logging.INFO)
+        
     # LOGフォルダがない場合は作成
     if not os.path.exists(LOG_DIR_NAME):
         os.mkdir(LOG_DIR_NAME)
@@ -15,10 +16,19 @@ def set_logger(name):
     # ログの設定
     now_time = datetime.now()
     formatter = logging.Formatter(LOG_FORMAT)
+    
+    # ファイル出力用のHandlerを設定
     fh = logging.FileHandler(filename=f'{LOG_DIR_NAME}/{now_time:log_%Y%m%d}.log',encoding="utf-8")
     fh.setFormatter(formatter)
     fh.setLevel=logging.INFO
 
-    logger.addHandler(fh)
+    # コンソール出力用のHandlerを設定
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    sh.setLevel=logging.INFO
 
+    # Logerに登録    
+    logger.addHandler(fh)
+    logger.addHandler(sh)
+    
     return logger
